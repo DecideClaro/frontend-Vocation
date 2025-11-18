bf34d75e071a313a081603075d40ec54c982db61
StanQ2 <gabito.correos@gmail.com>
2025-11-18 00:43:08 -0500

    Test vocacional
    Se creo un test vocacional, para probar el funcionamiento de la pagina

--- vocatio-frontend/src/app/app.routes.ts
+++ vocatio-frontend/src/app/app.routes.ts
@@ -9,6 +9,11 @@ export const routes: Routes = [
     canActivate: [authGuard],
     loadComponent: () => import('./features/home/home-page.component').then((m) => m.HomePageComponent)
   },
+  {
+    path: 'test',
+    canActivate: [authGuard],
+    loadComponent: () => import('./features/test/test-page/test-page').then((m) => m.TestPage)
+  },
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: '**', redirectTo: 'auth' }
 ];
